e73383383f6441c9da17a4cac8f0ea65b2dbca62
KauaMB2 <i.kauamoreirabatista@gmail.com>
2026-02-08 08:26:00 -0300

    backend

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,16 +1,34 @@
-import { StyleSheet, useColorScheme, View, ViewProps } from 'react-native'
+import { useColorScheme, View, ViewProps } from 'react-native'
 import { Colors } from '../constants/Colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-interface ThemedViewProps extends ViewProps {}
+interface ThemedViewProps extends ViewProps {
+  safe?: boolean;
+}
 
-const ThemedView = ({ style, ...props }: ThemedViewProps) => {
-    const colorScheme = useColorScheme()
-    const theme = Colors[colorScheme!] ?? Colors.light
+const ThemedView = ({ style, safe = false, ...props }: ThemedViewProps) => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme!] ?? Colors.light
+
+  if (!safe) return (
+    <View
+      style={[{ backgroundColor: theme.background }, style]}
+      {...props}
+    />
+  )
+
+  const insets = useSafeAreaInsets()
+
   return (
-    <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    <View 
+      style={[{ 
+        backgroundColor: theme.background,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+      }, style]} 
+      {...props}
+    />
   )
 }
 
 export default ThemedView
-
-const styles = StyleSheet.create({})
